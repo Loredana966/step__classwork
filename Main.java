52363ed59654f30e5e264a0f1945691f2e904e4d
Loredana966 <lchiperi10@gmail.com>
2024-01-29 21:50:18 +0200

    Add file main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+ 
+        //Declarați o variabilă de tip int și atribuiți-i o valoare.
+        int numarIntreg = 10;  
+        System.out.println("Variabila int: "+numarIntreg);
+
+        //Declarați o variabilă de tip double și atribuiți-i o valoare.          
+        double numarZecimal = 3.14;
+        System.out.println("Variabila double: "+numarZecimal);
+
+        //Declarați o variabilă de tip boolean și atribuiți-i o valoare.                 
+        boolean isTrue = true;
+        System.out.println("Variabila boolean: "+isTrue); 
+
+        //Declarați o variabilă de tip String și atribuiți-i o valoare.
+        String text = "Hello world";
+        System.out.println("Variabila string: "+text);
+
+
+        //Declarați două variabile int și efectuați adunarea, scaderea, inmultirea, impartirea între ele.
+        int nr1 = 8;
+        int nr2 = 4;
+
+        //Adunarea
+        int suma = nr1+nr2;
+        System.out.println("Suma int: "+suma);
+        //Scaderea
+        int diferenta = nr1-nr2;
+        System.out.println("Diferenta int: "+diferenta);
+        //Inmultirea
+        int produs = nr1*nr2;
+        System.out.println("Produs int: "+produs);
+        //Impartirea
+        int cat = nr1/nr2;
+        System.out.println("Impartire int: "+cat);
+             
+            
+    }
+}
